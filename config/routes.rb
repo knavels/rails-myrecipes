e2bcb820876238b8f33223f80f38ea968df4d6d6
Navid Dezashibi <dezashibi@gmail.com>
2020-07-28 16:05:37 +0430

    complete comments feature

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   # get 'recipes/new', to: 'recipes#new', as: 'new_recipe'
   # get 'recipes/:id', to: 'recipes#show', as: 'recipe'
   # I can use resource for this, so I do that
-  resources :recipes
+  resources :recipes do
+    resources :comments, only: [:create]
+  end
 
   get '/signup', to: 'chefs#new'
   resources :chefs, except: [:new]
